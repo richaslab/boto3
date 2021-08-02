9bb4b01a33726deb47a42aaa3fcd7ca99bd58ceb
Arunima Dhar <51229991+richaslab@users.noreply.github.com>
2021-08-02 19:51:43 +0530

    adding new file

---
+++ create s3.py
@@ -0,0 +1,10 @@
+import boto3
+aws_resource = boto3.resource("s3")
+bucket = aws_resource.Bucket("richasbucketfrompython")
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint':'us-east-2'
+    },
+)
+print(response)
